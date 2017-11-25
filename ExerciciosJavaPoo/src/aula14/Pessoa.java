6406b87bc51d2e880fffab60fb61fce659c770b8
claydiberto@gmail.com <claydiberto@Claydiberto-Macbook.local>
2017-11-24 21:40:22 -0300

    Inicio do projeto final do curso. Aula 14 concluida e testada.

---
+++ ExerciciosJavaPoo/src/aula14/Pessoa.java
@@ -0,0 +1,51 @@
+package aula14;
+
+public abstract class Pessoa {
+	protected String nome;
+	protected int idade;
+	protected String sexo;
+	protected float experiencia;
+	
+	protected Pessoa(String nome, int idade, String sexo) {
+		this.nome = nome;
+		this.idade = idade;
+		this.sexo = sexo;
+		this.experiencia = 0;
+	}
+	
+	protected void ganhaExp() {
+		this.experiencia ++;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public float getExperiencia() {
+		return experiencia;
+	}
+
+	public void setExperiencia(float experiencia) {
+		this.experiencia = experiencia;
+	}
+}
---
+++ ExerciciosJavaPoo/src/aula14/projetoYoutube.java
@@ -0,0 +1,12 @@
+package aula14;
+
+public class projetoYoutube {
+
+	public static void main(String[] args) {
+		Gafanhoto g1 = new Gafanhoto("Antonio", 25, "M", "ant");
+		System.out.println(g1.toString());
+		
+
+	}
+
+}
